e4543877497469d9060322d682afb0dc9a05e74b
gihyun <whrlgus33@gmail.com>
2020-04-29 18:51:02 +0900

    first commit

---
+++ AudioPlayer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AudioPlayer
+//
+//  Created by 조기현 on 2020/04/29.
+//  Copyright © 2020 none. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
